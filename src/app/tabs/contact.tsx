bbe925c3baaa358c7135bd2ff763714663674224
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-09-24 14:35:30 +0530

    moved files

---
+++ src/app/tabs/contact.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function Contact() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Contact Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});

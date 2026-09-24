cebc330356734ee85a826388ccb65bf0f5736686
Yasindu Dilshan <dilshanyasindu23@gmail.com>
2026-09-24 15:53:29 +0530

    created styles

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -11,8 +11,22 @@ export default function RootLayout() {
       headerTitleStyle: { fontWeight: 'bold' },
       tabBarIcon: ({ focused, color, size }) => {
         let iconName:any;
+
+        if(route.name === 'index') {
+          iconName = focused ? 'home' : 'home-outline';
+        } else if(route.name === 'about') {
+          iconName = focused ? 'information-circle' : 'information-circle-outline';
+        }else if(route.name === 'contact') {
+          iconName = focused ? 'call' : 'call-outline';
+        } 
+
+        return <Ionicons name={iconName} size={size} color={color} />;
       }
     })}>
+
+        <Tabs.Screen name="index" options={{ title: 'Home' }} />
+        <Tabs.Screen name="about" options={{ title: 'About' }} />
+        <Tabs.Screen name="contact" options={{ title: 'Contact' }} />
     </Tabs>
   );
 }
